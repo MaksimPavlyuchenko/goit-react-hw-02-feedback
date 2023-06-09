19df5c6961e43ba95f828eb1f77e71e477ec6a54
MaksimPavlyuchenko <maksimpavlyuchenko82@gmail.com>
2023-06-09 20:36:56 +0300

    task-one-two

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,5 +1,11 @@
 import { Component } from 'react';
-import {Header,ButtonBox,Button,CountItem,Statistic} from './Feedback.styled';
+import {
+  Header,
+  ButtonBox,
+  Button,
+  CountItem,
+  Statistic,
+} from './Feedback.styled';
 
 class Feedback extends Component {
   state = {
@@ -8,25 +14,49 @@ class Feedback extends Component {
     bad: 0,
   };
 
-  handleClick=(e)=>{
-    this.setState((prevState)=>{
-   return {[e.target.name]:prevState[e.target.name]+1}
-    })
-  }
-  
+  handleClick = e => {
+    this.setState(prevState => {
+      return { [e.target.name]: prevState[e.target.name] + 1 };
+    });
+  };
+  countTotalFeedback = () => {
+    const TotalCount = Object.values(this.state).reduce((total, number) => {
+      return total + number;
+    }, 0);
+    return TotalCount;
+  };
+  countPositiveFeedbackPercentage = () => {
+    const PositivePercents = (
+      (this.state.good / this.countTotalFeedback()) *
+      100
+    ).toFixed(0);
+
+    return PositivePercents;
+  };
+
   render() {
     return (
       <div>
         <Header>Please leave feedback</Header>
         <ButtonBox>
-        <Button type='button' onClick={this.handleClick} name='good'>Good</Button>
-        <Button type='button' onClick={this.handleClick} name='neutral'>Neutral</Button>
-        <Button type='button' onClick={this.handleClick} name='bad'>Bad</Button>
+          <Button type="button" onClick={this.handleClick} name="good">
+            Good
+          </Button>
+          <Button type="button" onClick={this.handleClick} name="neutral">
+            Neutral
+          </Button>
+          <Button type="button" onClick={this.handleClick} name="bad">
+            Bad
+          </Button>
         </ButtonBox>
         <Statistic>Statistics:</Statistic>
         <CountItem>Good: {this.state.good}</CountItem>
         <CountItem>Neutral: {this.state.neutral}</CountItem>
         <CountItem>Bad: {this.state.bad}</CountItem>
+        <CountItem>Total: {this.countTotalFeedback()}</CountItem>
+        <CountItem>
+          Positive feedback: {this.countPositiveFeedbackPercentage()}%
+        </CountItem>
       </div>
     );
   }
